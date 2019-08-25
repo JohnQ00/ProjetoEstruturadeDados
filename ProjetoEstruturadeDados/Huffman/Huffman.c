39c944c94ed54a3e85e6b0b9ddeb3e082c438be3
John Davi Dutra Canuto Pires <41549047+JohnQ00@users.noreply.github.com>
2019-08-25 19:48:13 -0300

    Update Huffman.c

--- ProjetoEstruturadeDados/Huffman/Huffman.c
+++ ProjetoEstruturadeDados/Huffman/Huffman.c
@@ -21,6 +21,11 @@ void counting_frequence(FILE *input, lint *frequence);
 void make_frequence(queue *tree_queue, lint *frequence);
 void compress(FILE *input, char *archive, queue *tree_queue);
 void construct_file(FILE *input, char *archive, tnode *huff_tree, hash * new_hash);
+int ascii(int dec);
+byte set_bit(byte aux, int i);
+int is_bit_set(byte aux, int i);
+int is_dualbit_set(dual_byte aux, int i);
+void put_tree_in_output(tnode *huff_tree, FILE *output);
 void count_tree_size(tnode *huff_tree, dual_byte *t);
 void enqueue(queue *tree_queue, byte c, lint freq, tnode *left, tnode *right);
 void make_huff_tree(queue *tree_queue);
@@ -91,6 +96,7 @@ struct hash
 struct element
 {
 	byte bin_counter[100];
+	int bin_size;
 };
 
 void set_frequence_to_zero(lint *frequence)
@@ -148,8 +154,8 @@ void compress(FILE *input, char *archive, queue *tree_queue)
 
 	hash *new_hash = create_dictionary();
 
-	tnode *hff = tree_queue->head;
-	print(hff);
+	//tnode *hff = tree_queue->head;
+	//print(hff);
 
 	tnode *huff_tree = tree_queue->head;
 	printf("Tree -> ");
@@ -158,7 +164,10 @@ void compress(FILE *input, char *archive, queue *tree_queue)
 
 	byte bin_counter[20] = {0};
 	lint i = 0;
+	int j = 0;
 	make_hash(new_hash, tree_queue->head, bin_counter, &i);
+	//for(i = 0; i < 256; i++)
+        //{for(j = 0;j < 20;j++){if(new_hash->table[i]->bin_counter != 0){printf("bin: %d\n", new_hash->table[i]->bin_counter[j]);}}}
 	print_dictionary(new_hash);
 
 	construct_file(input, &archive, huff_tree, new_hash);
@@ -166,30 +175,142 @@ void compress(FILE *input, char *archive, queue *tree_queue)
 
 void construct_file(FILE *input, char *archive, tnode *huff_tree, hash * new_hash)
 {
-	puts("construct");
-	int archive_name = strlen(archive);
+	//puts("construct");
+	//int archive_name = strlen(archive);
 
-	strcat(archive_name, ".huff");
+	//strcat(archive_name, ".huff");
 
-	int tree_size = 0;
+	dual_byte tree_size = 0;
 	count_tree_size(huff_tree, &tree_size);
 
+	FILE *output = fopen("compressed.huff", "w+b"); // escreve o arquivo em modo binário
+
+	fseek(output, 2 + (tree_size), SEEK_SET); // o inteiro no segundo parametro é o n de bytes, já quando coloca uma variável com tamanho é o numero de bits
+
+	byte c = 0;
+	byte aux = 0;
+	int index = 7; // serve pra montar cada byte no arquivo de saída, 0 - 7
+	int i;
+
+	// while(fscanf(input, "%c", &c) != EOF)
+	// {
+	// 	printf("%d", new_hash->table[c]->bin_size);
+	// }
+	while(fscanf(input, "%c", &c) != EOF)
+	{
+		for (i = 0; i < new_hash->table[c]->bin_size; ++i)
+		{
+		    printf("%d\n", new_hash->table[c]->bin_counter[i]);
+			if (ascii(new_hash->table[c]->bin_counter[i]) == 1)
+			{
+				aux = set_bit(aux,index);
+			}
+			index--;
+
+			if (index == -1)
+			{
+				fwrite(&aux, sizeof(byte),1, output);
+
+				aux = 0;
+
+				index = 7;
+			}
+		}
+	}
+
+
+    dual_byte trash = index + 1;
+    fwrite(&aux, sizeof(byte), 1, output);
+
+	rewind(output);
+
+/////////////////////////////////////////////////////////////////////////
+
+	dual_byte aux2 = trash;
+	aux2 = aux2 << 13;
+	//printf("\n%d\n", aux2);
+
 	printf("tree size: %d\n", tree_size);
+	aux2 = aux2 + tree_size;
+	printf("\nlixo: %d\n", aux2);
+	//printf("\n%d\n", aux2);
 
-	FILE *output = fopen(compressed.huff, "w+b"); // escreve o arquivo em modo binário
+	byte byte_one = 0;
+	byte byte_two = 0;
 
-	fseek(output, );
+	for(i = 15; i >= 8; i--)
+    {
+        if (is_dualbit_set(aux2, i))
+        {
+        	byte_one = set_bit(byte_one,i - 8);
+        }
+    }
+
+    for(i = 7; i >= 0 ; i--)
+    {
+        if (is_bit_set(aux2, i))
+        {
+        	byte_two = set_bit(byte_two,i);
+        }
+    }
+
+    printf("%d\n", byte_one);
+    printf("%d\n", byte_two);
+    fputc(byte_one, output);
+    fputc(byte_two, output);
+    put_tree_in_output(huff_tree,output);
+
+/////////////////////////////////////////////////////////////////////////
+}
+
+int ascii(int dec)
+{
+	if (dec == 49){return 1;}
+	else if(dec == 48){return 0;}
+}
+
+byte set_bit(byte aux, int i)
+{
+	byte mask = 1 << i;
+	return mask | aux;
+}
+
+int is_bit_set(byte aux, int i)
+{
+	byte mask = 1 << i;
+	return mask & aux;
+}
+
+int is_dualbit_set(dual_byte aux, int i)
+{
+	dual_byte mask = 1 << i;
+	return mask & aux;
+}
+
+void put_tree_in_output(tnode *huff_tree, FILE *output)
+{
+	if (huff_tree != NULL)
+	{
+		if ((huff_tree->c == '\\' || huff_tree->c == '*') && huff_tree->left == NULL && huff_tree->right == NULL )
+		{
+			fputc('\\', output);
+		}
+
+		fputc(huff_tree->c, output);
+		put_tree_in_output(huff_tree->left, output);
+		put_tree_in_output(huff_tree->right, output);
+	}
 }
 
 void count_tree_size(tnode *huff_tree, dual_byte *t)
 {
-	puts("count");
+
 	if (huff_tree != NULL)
 	{
-		// if ((huff_tree->c == '\\' || huff_tree->c == '*') && huff_tree->left == NULL && huff_tree->right == NULL )
-		// {
-		// 	*t += 1;
-		// }
+		if ((huff_tree->c == '\\' || huff_tree->c == '*') && huff_tree->left == NULL && huff_tree->right == NULL )
+		{
+			*t += 1;
+		}
 		*t += 1;
 
 		count_tree_size(huff_tree->left, t);
@@ -264,6 +385,8 @@ void make_hash(hash *ht, tnode *huff, byte *bin_counter, lint *i)
 void string_to_hash(hash *ht, byte index, byte *bin_counter)
 {
 	element *temp = (element*)malloc(sizeof(element));
+	temp->bin_size = strlen(bin_counter);
+	printf("%d \n", temp->bin_size);
 	strcpy(temp->bin_counter, bin_counter);
 	ht->table[index] = temp;
 }
